c545bfbd01f95230d2e9db5d55251765726447e5
nijakow <79372954+nijakow@users.noreply.github.com>
2022-05-16 00:32:15 +0200

    Added a reader

---
+++ src/reader.c
@@ -0,0 +1,105 @@
+#include <assert.h>
+#include <ctype.h>
+
+#include "reader.h"
+
+struct reader
+{
+  char* ptr;
+};
+
+char here(struct reader* reader)
+{
+  return *(reader->ptr);
+}
+
+void forward(struct reader* reader)
+{
+  reader->ptr++;
+};
+
+any lisp_reader_read_expr(struct reader*);
+
+void skip_whitespace(struct reader* reader)
+{
+  while (isspace(here(reader)))
+    forward(reader);
+}
+
+any lisp_reader_read_number(struct reader* reader)
+{
+  int v;
+
+  v = 0;
+  while (isdigit(here(reader)))
+    {
+      v = (v * 10) + (here(reader) - '0');
+      forward(reader);
+    }
+  return intref(v);
+}
+
+any lisp_reader_read_list(struct reader* reader)
+{
+  any v;
+
+  skip_whitespace(reader);
+  switch (here(reader))
+    {
+    case ')':
+      forward(reader);
+      return LISP_NIL;
+    case '.':
+      forward(reader);
+      v = lisp_reader_read_expr(reader);
+      skip_whitespace(reader);
+      assert(here(reader) == ')');
+      return v;
+    default:
+      v = lisp_reader_read_expr(reader);
+      return lisp_cons(v, lisp_reader_read_list(reader));
+    }
+}
+
+int is_special_char(char c)
+{
+  return isspace(c) || (c == ')');
+}
+
+any lisp_reader_read_symbol(struct reader* reader)
+{
+  unsigned int fill;
+  char buffer[1024];
+
+  fill = 0;
+  while (!is_special_char(here(reader))) /* TODO: Overflow check! */
+    {
+      buffer[fill++] = here(reader);
+      forward(reader);
+    }
+  buffer[fill++] = '\0';
+  return lisp_getsym(buffer);
+}
+
+any lisp_reader_read_expr(struct reader* reader)
+{
+  skip_whitespace(reader);
+  if (isdigit(here(reader)))
+    return lisp_reader_read_number(reader);
+  switch (here(reader))
+    {
+    case '(':
+      forward(reader);
+      return lisp_reader_read_list(reader);
+    default:
+      return lisp_reader_read_symbol(reader);
+    }
+}
+
+any lisp_reader_read(char* ptr)
+{
+  struct reader reader;
+
+  reader.ptr = ptr;
+  return lisp_reader_read_expr(&reader);
+}
---
+++ src/reader.h
@@ -0,0 +1,8 @@
+#ifndef NIL_READER_H
+#define NIL_READER_H
+
+#include "lisp.h"
+
+any lisp_reader_read(char*);
+
+#endif
